312e022e5ee1cce87a702450d5d5e581a1c23848
Tadhg-Henderson <tadhg.a.henderson@gmail.com>
2022-05-30 21:26:07 +1000

    website a-v0.12
    +clicking links now takes you to approppriate header
    -removed scrollD.js (uneeded)

--- Javascript/scrollF.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('scrollLink1').click(function(e) {
-        e.preventDefault();
-
-        $('html, body').animate({
-            scrollTop: $('#readings').offset().top
-        }, 500);
-    });
-});
