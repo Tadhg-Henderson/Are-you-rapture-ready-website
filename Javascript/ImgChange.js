ef5b7aa075ee1d30569365533d7dde6616833140
Tadhg-Henderson <tadhg.a.henderson@gmail.com>
2022-06-03 01:24:26 +1000

    website a-v0.13.11 fix +added baffle js library *changed paster to onclick

--- Javascript/ImgChange.js
+++ Javascript/ImgChange.js
@@ -1,9 +1,8 @@
 var image = document.getElementById("pastorImage")
 var delay = 3000;
-image.addEventListener('mouseover', function() {
-    setTimeout(function() {
-        image.src = "./Media/Images/pastor_creepy.jpg"
-    }, delay);
+
+image.addEventListener('click', function() {
+    image.src = "./Media/Images/pastor_creepy.jpg"
 })
 image.addEventListener('mouseout', function() {
     image.src = "./Media/Images/Screenshot 2022-05-20 at 02-07-20 website.png"
