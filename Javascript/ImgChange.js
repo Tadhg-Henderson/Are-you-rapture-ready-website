09aee1461f361967c44fd9a1a578c976c10a7b0e
Tadhg-Henderson <tadhg.a.henderson@gmail.com>
2022-06-12 22:54:38 +1000

    website a-v-0.14.0 big changes to themeing

--- Javascript/ImgChange.js
+++ Javascript/ImgChange.js
@@ -3,6 +3,10 @@ var delay = 3000;
 
 image.addEventListener('click', function() {
     image.src = "./Media/Images/pastor_creepy.jpg"
+    image.addEventListener('click', function() {
+        image.src = "./Media/Images/pastor_creepy2.jpg"
+    })
+    
 })
 image.addEventListener('mouseout', function() {
     image.src = "./Media/Images/Screenshot 2022-05-20 at 02-07-20 website.png"
