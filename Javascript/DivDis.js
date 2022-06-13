1630e09693ad8189982375490c77e7b942f69e00
Tadhg-Henderson <tadhg.a.henderson@gmail.com>
2022-06-14 07:26:50 +1000

    Website a-v0.14.1 +Changed name to fit thiming better +made certain elements disappear based on screen size  >added DivDis.js  +added many new photos  +ajusted background size  +changed font

---
+++ Javascript/DivDis.js
@@ -0,0 +1,9 @@
+var width = $( window ).width();
+console.log(width);
+if(width < 600)
+{
+   document.getElementById('disDiv').setAttribute('style', 'width:50%; display:none');
+   document.getElementById('disDivI').setAttribute('style', 'width:50%; display:none');
+   document.getElementById('chngDiv').setAttribute('style', 'width: 100%; text-align:center');
+   document.getElementById('chngDivI').setAttribute('style', 'width: 100%; text-align:center');
+}
--- Javascript/ImgChange.js
+++ Javascript/ImgChange.js
@@ -3,10 +3,11 @@ var delay = 3000;
 
 image.addEventListener('click', function() {
     image.src = "./Media/Images/pastor_creepy.jpg"
-    image.addEventListener('click', function() {
-        image.src = "./Media/Images/pastor_creepy2.jpg"
-    })
-    
+    setTimeout(function () {
+        image.addEventListener('mouseenter', function() {
+            image.src = "./Media/Images/pastor_creepy2.jpg"
+        })
+    } ,5000)
 })
 image.addEventListener('mouseout', function() {
     image.src = "./Media/Images/Screenshot 2022-05-20 at 02-07-20 website.png"
