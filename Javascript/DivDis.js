5255db61b40d77ee218210334585d663a2cf1c61
Tadhg-Henderson <tadhg.a.henderson@gmail.com>
2022-06-14 09:25:21 +1000

    Website a-v0.14.2 +clean up doc

--- Javascript/DivDis.js
+++ Javascript/DivDis.js
@@ -1,9 +1,9 @@
 var width = $( window ).width();
 console.log(width);
-if(width < 600)
+if(width < 800)
 {
    document.getElementById('disDiv').setAttribute('style', 'width:50%; display:none');
-   document.getElementById('disDivI').setAttribute('style', 'width:50%; display:none');
-   document.getElementById('chngDiv').setAttribute('style', 'width: 100%; text-align:center');
-   document.getElementById('chngDivI').setAttribute('style', 'width: 100%; text-align:center');
+   document.getElementById('disDivI').setAttribute('style', 'width:50%; display:none');;
+   document.getElementById('chngDiv').setAttribute('style', 'width:100%; text-align:center');
+   document.getElementById('chngDivI').setAttribute('style', 'width:100%; text-align:center');
 }
